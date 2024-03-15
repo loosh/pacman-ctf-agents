8fe24b106ead4f73114a40f5810652bc0f52e51d
loosh <notloosh@gmail.com>
2024-03-15 14:22:07 -0700

    Remove dummy agent

--- src/myTeam.py
+++ src/myTeam.py
@@ -62,49 +62,6 @@ def createTeam(firstIndex, secondIndex, isRed,
 # Agents #
 ##########
 
-class DummyAgent(CaptureAgent):
-  """
-  A Dummy agent to serve as an example of the necessary agent structure.
-  You should look at baselineTeam.py for more details about how to
-  create an agent as this is the bare minimum.
-  """
-
-  def registerInitialState(self, gameState):
-    """
-    This method handles the initial setup of the
-    agent to populate useful fields (such as what team
-    we're on).
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
-    IMPORTANT: This method may run for at most 15 seconds.
-    """
-
-    '''
-    Make sure you do not delete the following line. If you would like to
-    use Manhattan distances instead of maze distances in order to save
-    on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py.
-    '''
-    CaptureAgent.registerInitialState(self, gameState)
-
-    '''
-    Your initialization code goes here, if you need any.
-    '''
-
-
-  def chooseAction(self, gameState):
-    """
-    Picks among actions randomly.
-    """
-    actions = gameState.getLegalActions(self.index)
-
-    '''
-    You should change this in your own agent.
-    '''
-
-    return random.choice(actions)
-
 class OffensiveChaseAgent(CaptureAgent):
     """
     A specialized agent that focuses on chasing pellets in a capture-the-flag Pacman game.
@@ -286,7 +243,6 @@ class ImprovedOffensiveAgent(OffensiveChaseAgent):
             assert isinstance(value, (int, float)), f"Weight for {key} is not numeric: {value}"
 
         return weights
-
 
 
 class DynamicDefensiveAgent(DefensiveReflexAgent):
