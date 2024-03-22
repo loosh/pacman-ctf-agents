425d97c87aceb6240534de2b6e79544735ae29d9
arikanev <arikanevsky@gmail.com>
2024-03-22 12:44:35 -0700

    pellets

--- src/myTeam.py
+++ src/myTeam.py
@@ -1,187 +1,140 @@
+ # myTeam.py
+ # ---------
+ # Licensing Information:  You are free to use or extend these projects for
+ # educational purposes provided that (1) you do not distribute or publish
+ # solutions, (2) you retain this notice, and (3) you provide clear
+ # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+ # 
+ # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+ # The core projects and autograders were primarily created by John DeNero
+ # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+ # Student side autograding was added by Brad Miller, Nick Hay, and
+ # Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
 from captureAgents import CaptureAgent
-import random, util
+import random, time, util
 from game import Directions
-import numpy as np
-from util import nearestPoint, manhattanDistance
+import game
 
 ##################
 # Game Constants #
 ##################
 
+# Set TRAINING to True while agents are learning, False if in deployment
+# [!] Submit your final team with this set to False!
 TRAINING = True
+
+# Name of weights / any agent parameters that should persist between
+# games. Should be loaded at the start of any game, training or otherwise
+# [!] Replace MY_TEAM with your team name
 WEIGHT_PATH = 'weights_MY_TEAM.json'
 
+# Any other constants used for your training (learning rate, discount, etc.)
+# should be specified here
+# [!] TODO
+
 #################
-# Team Creation #
+# Team creation #
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first='QLearningOffensiveAgent', second='QLearningDefensiveAgent'):
-    return [eval(first)(firstIndex), eval(second)(secondIndex)]
+              first = 'PelletChaserAgent', second = 'PelletChaserAgent'):
+ """
+ This function should return a list of two agents that will form the
+ team, initialized using firstIndex and secondIndex as their agent
+ index numbers.  isRed is True if the red team is being created, and
+ will be False if the blue team is being created.
+ As a potentially helpful development aid, this function can take
+ additional string-valued keyword arguments ("first" and "second" are
+ such arguments in the case of this function), which will come from
+ the --redOpts and --blueOpts command-line arguments to capture.py.
+ For the nightly contest, however, your team will be created without
+ any extra arguments, so you should make sure that the default
+ behavior is what you want for the nightly contest.
+ """
 
-class FoundationAgent(CaptureAgent):
-    def __init__(self, index, epsilon=0.05, alpha=0.2, gamma=0.8):
-        super().__init__(index)
-        self.epsilon = epsilon
-        self.alpha = alpha
-        self.gamma = gamma
-        self.weights = util.Counter()
-        self.lastPositions = []
-        self.repeatedActions = {}
+ # The following line is an example only; feel free to change it.
+ return [eval(first)(firstIndex), eval(second)(secondIndex)]
 
+##########
+# Agents #
+##########
+
+class DummyAgent(CaptureAgent):
+ """
+ A Dummy agent to serve as an example of the necessary agent structure.
+ You should look at baselineTeam.py for more details about how to
+ create an agent as this is the bare minimum.
+ """
+
+ def registerInitialState(self, gameState):
+   """
+   This method handles the initial setup of the
+   agent to populate useful fields (such as what team
+   we're on).
+   A distanceCalculator instance caches the maze distances
+   between each pair of positions, so your agents can use:
+   self.distancer.getDistance(p1, p2)
+   IMPORTANT: This method may run for at most 15 seconds.
+   """
+
+   '''
+   Make sure you do not delete the following line. If you would like to
+   use Manhattan distances instead of maze distances in order to save
+   on initialization time, please take a look at
+   CaptureAgent.registerInitialState in captureAgents.py.
+   '''
+   CaptureAgent.registerInitialState(self, gameState)
+
+   '''
+   Your initialization code goes here, if you need any.
+   '''
+
+
+ def chooseAction(self, gameState):
+   """
+   Picks among actions randomly.
+   """
+   actions = gameState.getLegalActions(self.index)
+
+   '''
+   You should change this in your own agent.
+   '''
+
+   return random.choice(actions)
+
+
+
+class PelletChaserAgent(CaptureAgent):
     def registerInitialState(self, gameState):
-        super().registerInitialState(gameState)
-        self.start = gameState.getAgentPosition(self.index)
-        self.lastPositions = [self.start for _ in range(5)]
-
-    def getQValue(self, gameState, action):
-        features = self.getFeatures(gameState, action)
-        return sum(self.weights[f] * features[f] for f in features)
-
-    def update(self, gameState, action, nextState, reward):
-        features = self.getFeatures(gameState, action)
-        difference = (reward + self.gamma * self.getValue(nextState)) - self.getQValue(gameState, action)
-        for feature, value in features.items():
-            self.weights[feature] += self.alpha * difference * value
+        CaptureAgent.registerInitialState(self, gameState)
+        self.startPosition = gameState.getAgentPosition(self.index)
 
     def chooseAction(self, gameState):
         actions = gameState.getLegalActions(self.index)
-        if util.flipCoin(self.epsilon):
-            return random.choice(actions)
-        values = [self.getQValue(gameState, a) for a in actions]
-        maxValue = max(values)
-        bestActions = [a for a, v in zip(actions, values) if v == maxValue]
-        return random.choice(bestActions)
 
-    def getValue(self, gameState):
-        actions = gameState.getLegalActions(self.index)
-        if not actions:
-            return 0.0
-        values = [self.getQValue(gameState, a) for a in actions]
-        return max(values)
+        # Get the current position of the agent
+        currentPosition = gameState.getAgentPosition(self.index)
 
-    def getFeatures(self, gameState, action):
-        features = util.Counter()
-        features['bias'] = 1.0
-        return features
+        # Get the positions of the pellets
+        pelletPositions = self.getFood(gameState).asList()
 
-    def getSuccessor(self, gameState, action):
-        return gameState.generateSuccessor(self.index, action)
+        # If there are pellets available, find the nearest one
+        if pelletPositions:
+            nearestPellet = min(pelletPositions, key=lambda x: self.getMazeDistance(currentPosition, x))
 
-class QLearningOffensiveAgent(FoundationAgent):
-    def __init__(self, index, epsilon=0.05, alpha=0.2, gamma=0.8, optimisticSampling=True):
-        super().__init__(index, epsilon, alpha, gamma)
-        self.weights = util.Counter({
-            'distanceToFood': -10,  # Encourage moving towards food
-            'distanceToGhost': 20,  # Discourage getting close to ghosts
-            'stop': -100,
-            'reverse': -20,
-            'numCarrying': 5,
-            'distanceToSafeZone': -1,
-            'distanceFromStart': 20,
-            'stuckPenalty': -400,
-            'exploreBias': 300,
-            'scaredDistanceToEnemy': 10,
-            'returnToFriendlySide': 50,  # Encourage returning to friendly side when carrying pellets
-        })
-        self.optimisticSampling = optimisticSampling
+            # Find the action that moves the agent closest to the nearest pellet
+            bestAction = None
+            bestDistance = float('inf')
+            for action in actions:
+                successorPosition = gameState.generateSuccessor(self.index, action).getAgentPosition(self.index)
+                distance = self.getMazeDistance(successorPosition, nearestPellet)
+                if distance < bestDistance:
+                    bestAction = action
+                    bestDistance = distance
 
-    def getFeatures(self, gameState, action):
-        features = super().getFeatures(gameState, action)
-        successor = self.getSuccessor(gameState, action)
-        myState = successor.getAgentState(self.index)
-        myPos = myState.getPosition()
+            return bestAction
 
-        # Compute distance to the nearest food
-        foodList = self.getFood(successor).asList()
-        if len(foodList) > 0:
-            minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
-            features['distanceToFood'] = minDistance
-
-        # Compute distance to the nearest ghost
-        enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
-        defenders = [a for a in enemies if not a.isPacman and a.getPosition() is not None]
-        if len(defenders) > 0:
-            distances = [self.getMazeDistance(myPos, a.getPosition()) for a in defenders]
-            features['distanceToGhost'] = min(distances)
-
-        features['numCarrying'] = myState.numCarrying
-        if myState.numCarrying > 0:
-            features['distanceToSafeZone'] = self.getMazeDistance(myPos, self.start)
-            # Encourage returning to the friendly side when carrying pellets
-            middleX = gameState.data.layout.width // 2
-            if self.red:
-                if myPos[0] <= middleX:
-                    features['returnToFriendlySide'] = 1
-            else:
-                if myPos[0] >= middleX:
-                    features['returnToFriendlySide'] = 1
-
-        features['distanceFromStart'] = self.getMazeDistance(myPos, self.start)
-
-        if self.lastPositions[-5:].count(myPos) == 5:
-            features['stuckPenalty'] = 1
-
-        if len(self.lastPositions) > 5 and self.lastPositions[-1] == self.lastPositions[-3] and self.lastPositions[-2] == self.lastPositions[-4]:
-            features['exploreBias'] = 1
-
-        if myState.scaredTimer > 0:
-            enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
-            enemyPacmen = [a for a in enemies if a.isPacman and a.getPosition() is not None]
-            if enemyPacmen:
-                minDistance = min(self.getMazeDistance(myPos, a.getPosition()) for a in enemyPacmen)
-                features['scaredDistanceToEnemy'] = minDistance
-
-        return features
-
-class QLearningDefensiveAgent(FoundationAgent):
-    def __init__(self, index, epsilon=0.05, alpha=0.2, gamma=0.8, optimisticSampling=True):
-        super().__init__(index, epsilon, alpha, gamma)
-        self.weights = util.Counter({
-            'onDefense': 100,
-            'invadersDistance': -10,
-            'stop': -100,
-            'reverse': -20,
-            'numInvaders': -10,
-            'distanceFromStart': 20,
-            'stuckPenalty': -200,
-            'exploreBias': 200,
-            'scaredDistanceToEnemy': 10,  # Added feature for scared state
-        })
-        self.optimisticSampling = optimisticSampling
-
-    def getFeatures(self, gameState, action):
-        features = super().getFeatures(gameState, action)
-        successor = self.getSuccessor(gameState, action)
-        myState = successor.getAgentState(self.index)
-        myPos = myState.getPosition()
-
-        features['onDefense'] = 1 if not myState.isPacman else 0
-        enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
-        invaders = [a for a in enemies if a.isPacman and a.getPosition() is not None]
-        features['numInvaders'] = len(invaders)
-        dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders] if invaders else [0]
-        features['invadersDistance'] = min(dists, default=0)
-
-        middleX = gameState.data.layout.width // 2
-        if self.red:
-            features['avoidEnemySide'] = -100 if myPos[0] >= middleX else 1
-        else:
-            features['avoidEnemySide'] = -100 if myPos[0] <= middleX else 1
-
-        features['distanceFromStart'] = self.getMazeDistance(myPos, self.start)
-
-        if self.lastPositions[-5:].count(myPos) == 5:
-            features['stuckPenalty'] = 1
-
-        if len(self.lastPositions) > 5 and self.lastPositions[-1] == self.lastPositions[-3] and self.lastPositions[-2] == self.lastPositions[-4]:
-            features['exploreBias'] = 1
-
-        if myState.scaredTimer > 0:
-            enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
-            enemyPacmen = [a for a in enemies if a.isPacman and a.getPosition() is not None]
-            if enemyPacmen:
-                minDistance = min(self.getMazeDistance(myPos, a.getPosition()) for a in enemyPacmen)
-                features['scaredDistanceToEnemy'] = minDistance
-
-        return features
+        # If there are no pellets available, return a random action
+        return random.choice(actions)
